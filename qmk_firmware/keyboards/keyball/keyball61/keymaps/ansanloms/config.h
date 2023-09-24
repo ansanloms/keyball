557f00170cb07726f58c074372f8b010f198b57b
ansanloms <ansanloms@users.noreply.github.com>
2023-09-24 14:04:49 +0900

    fix: マウスレイヤーから戻る時間を調整。

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/ansanloms/config.h
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/ansanloms/config.h
@@ -39,4 +39,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // Automatic Mouse Layer
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 4
-#define AUTO_MOUSE_TIME 480
+#define AUTO_MOUSE_TIME 860
